d2cfc728b13a5e28ad44db1f20428c6c993f3314
Nandhakumar2005 <nandhakumarks2028@cs.sjcetpalai.ac.in>
2024-11-21 21:20:40 +0530

    Add files via upload

---
+++ day6/calculate.py
@@ -0,0 +1,13 @@
+'''
+NAME:Nandhakumar KS
+DATE:21-11-2024
+Write a python program to ask whether the given number is positive, negative or zero.
+'''
+
+number = int(input("Enter a number: "))
+if number > 0 :
+    print("The number is positive.")
+elif number < 0 :
+    print("The number is negative.")
+else:
+    print("The number is zero.")
