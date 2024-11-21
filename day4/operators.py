6d7c4a8b18ebb1e43d4557236cf40ecfe4fef5d7
Nandhakumar2005 <nandhakumarks2028@cs.sjcetpalai.ac.in>
2024-11-21 20:50:40 +0530

    Add files via upload

---
+++ day4/operators.py
@@ -0,0 +1,22 @@
+'''
+NAME:Nandhakumar KS
+DATE:21-11-2024
+Write a python program to print your name as output.
+'''
+
+num1 = 10
+num2 = 5
+#Addition
+print(num1 + num2)
+#Substraction
+print(num1 - num2)
+#Multiplication
+print(num1 * num2)
+#Division
+print(num1 / num2)
+#Modulus
+print(num1%num2)
+#Exponentiation
+print(num1**num2)
+#Floor division
+print(num1 // num2)
