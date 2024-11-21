d9ae486419f67b4be39c569b1b3d73a960fff9fd
Nandhakumar2005 <nandhakumarks2028@cs.sjcetpalai.ac.in>
2024-11-21 21:58:51 +0530

    Add files via upload

---
+++ day11/day 11 Nandhakumar KS.py
@@ -0,0 +1,13 @@
+'''
+NAME: Nandhakumar KS
+DATE:21-11-2024
+Count occurences of 'a' and 'c' in a string.
+'''
+
+string = "academic"
+char_list = list(string)
+print(char_list)
+count_a = string.count("a")
+print("Count of 'a' in the word academic is: ", count_a)
+count_c = string.count("c")
+print("Count of 'c' in the word academic is: ", count_c)
