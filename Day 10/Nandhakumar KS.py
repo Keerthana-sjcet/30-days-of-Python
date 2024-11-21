5db2327fe29e1e89d0f4e5e28e96e53d4dffdc84
Nandhakumar2005 <nandhakumarks2028@cs.sjcetpalai.ac.in>
2024-11-21 21:51:48 +0530

    Add files via upload

---
+++ Day 10/Nandhakumar KS.py
@@ -0,0 +1,16 @@
+'''
+NAME: Nandhakumar KS
+DATE: 21-11-2024
+To find the output of the following codes.
+'''
+
+#1
+x = 5
+y = "5"
+print(x==y)#output = false
+#2
+print(2**3**2)#output = 512
+#3
+print("Hello,World!"[::2])#output = HloWrd
+#4
+list = [1,2,3,4,5]
