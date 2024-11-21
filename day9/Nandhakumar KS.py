2b90a6c798b5ab831b230d18194043ff992e0a24
Nandhakumar2005 <nandhakumarks2028@cs.sjcetpalai.ac.in>
2024-11-21 21:43:27 +0530

    Add files via upload

---
+++ day9/Nandhakumar KS.py
@@ -0,0 +1,18 @@
+'''
+NAME: Nandhakumar KS
+DATE: 21-11-2024
+To familiarise with list.
+'''
+
+#1
+numbers = [4,7,1,9,12,3]
+numbers.sort()
+print(numbers)
+#2
+numbers = [4,7,1,9,12,3]
+largest = max(numbers)
+print('The largest number is', largest)
+#3
+numbers = [1,2,3,4,2,5,2,6]
+count_of_two = numbers.count(2)
+print(count_of_two)
