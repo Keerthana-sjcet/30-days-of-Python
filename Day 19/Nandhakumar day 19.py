e9732de48a1bab185e7328f225187b118f91149a
Nandhakumar2005 <nandhakumarks2028@cs.sjcetpalai.ac.in>
2024-12-20 20:39:18 +0530

    Add files via upload

---
+++ Day 19/Nandhakumar day 19.py
@@ -0,0 +1,9 @@
+'''
+NAME : Nandhakumar KS
+DATE : 20-12-2024
+Write a program in python to exchange its first and last elements
+'''
+
+list =['python','java','c++','c']
+new_list =[list[-1],list[1],list[2],list[0]]
+print(new_list)
